237ae2c222db548843274dc99e1f1bec37a693f4
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-17 11:55:33 +0100

    ✨feat(procfile): adding procfile for heroku

--- quickstart-react-rails-app/src/apis/registrations.js
+++ quickstart-react-rails-app/src/apis/registrations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const baseUrl = 'http://localhost:3001/';
-const baseUrl = 'https://react-rails-app-ready.herokuapp.com/';
+const baseUrl = 'https://react-testing-deploying.herokuapp.com/';
 
 const updatePassword = payload =>
   axios.put(`${baseUrl}api/v1/password/update`, payload);
